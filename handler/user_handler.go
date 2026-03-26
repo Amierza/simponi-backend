5b51f65eb096f77d178e6a3a99678b7fd7cb9f3c
Reyysusanto <rheinaldysusanto@gmail.com>
2026-03-26 09:50:52 +0700

    feat: logging feature

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -14,6 +14,11 @@ import (
 type (
 	IUserHandler interface {
 		GetProfile(ctx *gin.Context)
+		GetLogs(ctx *gin.Context)
+		GetLogsByStoreID(ctx *gin.Context)
+		GetLogsByDateRange(ctx *gin.Context)
+
+		CreateLog(ctx *gin.Context)
 	}
 
 	userHandler struct {
@@ -29,16 +34,78 @@ func NewUserHandler(userService service.IUserService, logger *zap.Logger) *userH
 	}
 }
 
-
-func (uh *userHandler) GetProfile(ctx *gin.Context){
+func (uh *userHandler) GetProfile(ctx *gin.Context) {
 	result, err := uh.userService.GetProfile(ctx)
 	if err != nil {
 		status := mapErrorStatus(err)
 		res := response.BuildResponseFailed(fmt.Sprintf("%s user", dto.FAILED_GET_PROFILE), err.Error(), nil)
 		ctx.AbortWithStatusJSON(status, res)
-		return 
+		return
 	}
 
 	res := response.BuildResponseSuccess(fmt.Sprintf("%s user", dto.SUCCESS_GET_PROFILE), result)
+	ctx.JSON(http.StatusOK, res)
+}
+
+func (uh *userHandler) GetLogs(ctx *gin.Context) {
+	result, err := uh.userService.GetLogs(ctx)
+	if err != nil {
+		status := mapErrorStatus(err)
+		res := response.BuildResponseFailed(fmt.Sprintf("%s logs", dto.FAILED_GET_LOGS), err.Error(), nil)
+		ctx.AbortWithStatusJSON(status, res)
+		return
+	}
+
+	res := response.BuildResponseSuccess(fmt.Sprintf("%s logs", dto.SUCCESS_GET_LOGS), result)
+	ctx.JSON(http.StatusOK, res)
+}
+
+func (uh *userHandler) GetLogsByStoreID(ctx *gin.Context) {
+	storeID := ctx.Param("storeID")
+	result, err := uh.userService.GetLogsByStoreID(ctx, storeID)
+	if err != nil {
+		status := mapErrorStatus(err)
+		res := response.BuildResponseFailed(fmt.Sprintf("%s logs by store ID", dto.FAILED_GET_LOGS_BY_STORE_ID), err.Error(), nil)
+		ctx.AbortWithStatusJSON(status, res)
+		return
+	}
+
+	res := response.BuildResponseSuccess(fmt.Sprintf("%s logs by store ID", dto.SUCCESS_GET_LOGS_BY_STORE_ID), result)
+	ctx.JSON(http.StatusOK, res)
+}
+
+func (uh *userHandler) GetLogsByDateRange(ctx *gin.Context) {
+	startDate := ctx.Query("start_date")
+	endDate := ctx.Query("end_date")
+	result, err := uh.userService.GetLogsByDateRange(ctx, startDate, endDate)
+	if err != nil {
+		status := mapErrorStatus(err)
+		res := response.BuildResponseFailed(fmt.Sprintf("%s logs by date range", dto.FAILED_GET_LOGS_BY_DATE_RANGE), err.Error(), nil)
+		ctx.AbortWithStatusJSON(status, res)
+		return
+	}
+
+	res := response.BuildResponseSuccess(fmt.Sprintf("%s logs by date range", dto.SUCCESS_GET_LOGS_BY_DATE_RANGE), result)
+	ctx.JSON(http.StatusOK, res)
+}
+
+func (uh *userHandler) CreateLog(ctx *gin.Context) {
+	var req dto.LoggingRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		status := mapErrorStatus(err)
+		res := response.BuildResponseFailed(fmt.Sprintf("%s log", dto.FAILED_CREATE_LOG), err.Error(), nil)
+		ctx.AbortWithStatusJSON(status, res)
+		return
+	}
+
+	result, err := uh.userService.CreateLog(ctx, req)
+	if err != nil {
+		status := mapErrorStatus(err)
+		res := response.BuildResponseFailed(fmt.Sprintf("%s log", dto.FAILED_CREATE_LOG), err.Error(), nil)
+		ctx.AbortWithStatusJSON(status, res)
+		return
+	}
+
+	res := response.BuildResponseSuccess(fmt.Sprintf("%s log", dto.SUCCESS_CREATE_LOG), result)
 	ctx.JSON(http.StatusOK, res)
 }
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -10,7 +10,14 @@ import (
 
 type (
 	IUserRepository interface {
+		// Get
 		GetUserByID(ctx context.Context, tx *gorm.DB, id string) (*entity.User, bool, error)
+		GetLogByStoreID(ctx context.Context, tx *gorm.DB, storeID string) ([]entity.Log, bool, error)
+		GetLogByDateRange(ctx context.Context, tx *gorm.DB, startDate, endDate string) ([]entity.Log, bool, error)
+		GetAllLogs(ctx context.Context, tx *gorm.DB) ([]entity.Log, bool, error)
+
+		// Create
+		CreateLog(ctx context.Context, tx *gorm.DB, log *entity.Log) (*entity.Log, error)
 	}
 
 	userRepository struct {
@@ -24,14 +31,14 @@ func NewUserRepository(db *gorm.DB) *userRepository {
 	}
 }
 
-func (ur *userRepository) GetUserByID(ctx context.Context, tx *gorm.DB, id string) (*entity.User, bool, error){
+func (ur *userRepository) GetUserByID(ctx context.Context, tx *gorm.DB, id string) (*entity.User, bool, error) {
 	if tx == nil {
 		tx = ur.db
 	}
 
 	user := new(entity.User)
 	err := tx.WithContext(ctx).Where("id = ?", id).Take(user).Error
-	if errors.Is(err, gorm.ErrRecordNotFound){
+	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, false, nil
 	}
 
@@ -40,4 +47,58 @@ func (ur *userRepository) GetUserByID(ctx context.Context, tx *gorm.DB, id strin
 	}
 
 	return user, true, nil
+}
+
+func (ur *userRepository) GetLogByStoreID(ctx context.Context, tx *gorm.DB, storeID string) ([]entity.Log, bool, error) {
+	if tx == nil {
+		tx = ur.db
+	}
+
+	var logs []entity.Log
+
+	if err := tx.WithContext(ctx).Model(&entity.Log{}).Where("store_id = ?", storeID).Find(&logs).Error; err != nil {
+		return nil, false, err
+	}
+
+	return logs, true, nil
+}
+
+func (ur *userRepository) GetLogByDateRange(ctx context.Context, tx *gorm.DB, startDate, endDate string) ([]entity.Log, bool, error) {
+	if tx == nil {
+		tx = ur.db
+	}
+
+	var logs []entity.Log
+
+	if err := tx.WithContext(ctx).Model(&entity.Log{}).Where("created_at BETWEEN ? AND ?", startDate, endDate).Find(&logs).Error; err != nil {
+		return nil, false, err
+	}
+
+	return logs, true, nil
+}
+
+func (ur *userRepository) GetAllLogs(ctx context.Context, tx *gorm.DB) ([]entity.Log, bool, error) {
+	if tx == nil {
+		tx = ur.db
+	}
+
+	var logs []entity.Log
+
+	if err := tx.WithContext(ctx).Model(&entity.Log{}).Find(&logs).Error; err != nil {
+		return nil, false, err
+	}
+
+	return logs, true, nil
+}
+
+func (ur *userRepository) CreateLog(ctx context.Context, tx *gorm.DB, log *entity.Log) (*entity.Log, error) {
+	if tx == nil {
+		tx = ur.db
+	}
+
+	if err := tx.WithContext(ctx).Create(log).Error; err != nil {
+		return nil, err
+	}
+
+	return log, nil
 }
