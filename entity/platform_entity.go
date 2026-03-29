d4ec9a6080532f184bd2910eb616a09c20a11277
pilemon31 <barimbingpilemon@gmail.com>
2026-03-29 21:14:45 +0700

    feat: fix entity for product external

--- entity/platform_entity.go
+++ entity/platform_entity.go
@@ -3,10 +3,10 @@ package entity
 import "github.com/google/uuid"
 
 type Platform struct {
-	ID   uuid.UUID `gorm:"type:uuid;primaryKey"`
-	Name string    `gorm:"uniqueIndex"` // shopee, tiktok
+	ID   uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Name string    `gorm:"uniqueIndex" json:"name"` // shopee, tiktok
 
-	Stores []*Store `gorm:"foreignKey:PlatformID"`
+	Stores []*Store `gorm:"foreignKey:PlatformID" json:"stores,omitempty"`
 
 	TimeStamp
 }
