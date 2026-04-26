b6f51f15c2823b718243f335a387840b0ce42e0b
pilemon31 <barimbingpilemon@gmail.com>
2026-04-26 14:52:33 +0700

    refactor code

--- routes/external_product_route.go
+++ routes/external_product_route.go
@@ -8,15 +8,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func ExternalProduct(route *gin.Engine, externalProductHandler handler.IExternalProductHandler, jwtService jwt.IJWT, permissionService repository.IRolePermissionRepository) {
+func ExternalProduct(route *gin.Engine, externalProductHandler handler.IExternalProductHandler, jwtService jwt.IJWT, rolePermissionRepo repository.IRolePermissionRepository) {
 	routes := route.Group("/api/v1/external-products").Use(middleware.Authentication(jwtService))
 	{
-		routes.GET("/product/:productId", middleware.RBAC(permissionService, "GetExternalProductsByProductID"), externalProductHandler.GetExternalProductsByProductID)
-		routes.GET("/store-platform/:storePlatformId", middleware.RBAC(permissionService, "GetExternalProductsByStorePlatformID"), externalProductHandler.GetExternalProductsByStorePlatformID)
-		routes.POST("/", middleware.RBAC(permissionService, "CreateExternalProduct"), externalProductHandler.CreateExternalProduct)
-		routes.GET("/", middleware.RBAC(permissionService, "GetExternalProducts"), externalProductHandler.GetExternalProducts)
-		routes.GET("/:id", middleware.RBAC(permissionService, "GetExternalProductByID"), externalProductHandler.GetExternalProductByID)
-		routes.PUT("/:id", middleware.RBAC(permissionService, "UpdateExternalProduct"), externalProductHandler.UpdateExternalProduct)
-		routes.DELETE("/:id", middleware.RBAC(permissionService, "DeleteExternalProductByID"), externalProductHandler.DeleteExternalProductByID)
+		routes.GET("/product/:productId", middleware.RBAC(rolePermissionRepo, "GetExternalProductsByProductID"), externalProductHandler.GetExternalProductsByProductID)
+		routes.GET("/store-platform/:storePlatformId", middleware.RBAC(rolePermissionRepo, "GetExternalProductsByStorePlatformID"), externalProductHandler.GetExternalProductsByStorePlatformID)
+		routes.POST("/", middleware.RBAC(rolePermissionRepo, "CreateExternalProduct"), externalProductHandler.CreateExternalProduct)
+		routes.GET("/", middleware.RBAC(rolePermissionRepo, "GetExternalProducts"), externalProductHandler.GetExternalProducts)
+		routes.GET("/:id", middleware.RBAC(rolePermissionRepo, "GetExternalProductByID"), externalProductHandler.GetExternalProductByID)
+		routes.PUT("/:id", middleware.RBAC(rolePermissionRepo, "UpdateExternalProduct"), externalProductHandler.UpdateExternalProduct)
+		routes.DELETE("/:id", middleware.RBAC(rolePermissionRepo, "DeleteExternalProductByID"), externalProductHandler.DeleteExternalProductByID)
 	}
 }
